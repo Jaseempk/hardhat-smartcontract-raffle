9a28170935340f6cfd6aea5f47bf2a66ecc15c16
Jaseempk <jasimpk011@gmail.com>
2022-11-18 09:42:42 +0530

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,5 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
 //require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-deploy");
@@ -6,92 +8,32 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
-const GOERLI_RPC_URL =
-    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key";
-
-const PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY || "0x";
-// optional
-//const MNEMONIC = process.env.MNEMONIC || "your mnemonic";
-
-// Your API key for Etherscan, obtain one at https://etherscan.io/
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
-//const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key";
-//const REPORT_GAS = process.env.REPORT_GAS || false;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_KEY = process.env.COINMARKETCAP_KEY;
 
 module.exports = {
+    solidity: "0.8.7",
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
-            // // If you want to do some forking, uncomment this
-            // forking: {
-            //   url: MAINNET_RPC_URL
-            // }
             chainId: 31337,
-        },
-        localhost: {
-            chainId: 31337,
+            blockConfirmations: 6,
+            //accounts:[PRIVATE_KEY]
         },
         goerli: {
-            url: GOERLI_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            //   accounts: {
-            //     mnemonic: MNEMONIC,
-            //   },
-            saveDeployments: true,
             chainId: 5,
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
         },
     },
-    etherscan: {
-        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-        apiKey: {
-            goerli: ETHERSCAN_API_KEY,
-        },
-        customChains: [
-            {
-                network: "goerli",
-                chainId: 5,
-                urls: {
-                    apiURL: "https://api-goerli.etherscan.io/api",
-                    browserURL: "https://goerli.etherscan.io",
-                },
-            },
-        ],
-    },
-    gasReporter: {
-        enabled: true,
-        currency: "USD",
-        outputFile: "gas-report.txt",
-        noColors: true,
-        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-    },
-    contractSizer: {
-        runOnCompile: false,
-        only: ["Raffle"],
-    },
-    namedAccounts: {
+    getNamedAccounts: {
         deployer: {
-            default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+            default: 0,
         },
         player: {
             default: 1,
         },
-    },
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.7",
-            },
-            {
-                version: "0.4.24",
-            },
-        ],
-    },
-    mocha: {
-        timeout: 500000, // 500 seconds max for running tests
     },
 };
